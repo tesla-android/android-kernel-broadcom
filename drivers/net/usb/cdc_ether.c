9ed9e73b6e02af509fd3095a7a3baf521a3a5a11
Michał Gapiński <mike@gapinski.eu>
2022-06-21 17:54:50 +0200

    cdc_ether: Map USB network cards as ethX interfaces in order to allow tethering alongside normal network interfaces on Android
    Signed-off-by: Michał Gapiński <mike@gapinski.eu>

--- drivers/net/usb/cdc_ether.c
+++ drivers/net/usb/cdc_ether.c
@@ -522,7 +522,7 @@ static void usbnet_cdc_zte_status(struct usbnet *dev, struct urb *urb)
 
 static const struct driver_info	cdc_info = {
 	.description =	"CDC Ethernet Device",
-	.flags =	FLAG_ETHER | FLAG_POINTTOPOINT,
+	.flags =	FLAG_ETHER,
 	.bind =		usbnet_cdc_bind,
 	.unbind =	usbnet_cdc_unbind,
 	.status =	usbnet_cdc_status,
@@ -532,7 +532,7 @@ static const struct driver_info	cdc_info = {
 
 static const struct driver_info	zte_cdc_info = {
 	.description =	"ZTE CDC Ethernet Device",
-	.flags =	FLAG_ETHER | FLAG_POINTTOPOINT,
+	.flags =	FLAG_ETHER,
 	.bind =		usbnet_cdc_zte_bind,
 	.unbind =	usbnet_cdc_unbind,
 	.status =	usbnet_cdc_zte_status,
